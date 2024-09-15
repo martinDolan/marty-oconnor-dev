35625bbd3a5d8982ee7baf208eda51b4f543afdd
martinDolan <martyopants@gmail.com>
2024-09-15 18:38:18 -0400

    Change enqueue to not use parent enqueue

--- themes/marty-oconnor-frontend-engineer/functions.php
+++ themes/marty-oconnor-frontend-engineer/functions.php
@@ -4,15 +4,24 @@
  * Enqueue theme assets.
  */
 
+// Enqueue styles and scripts for the frontend.
 function theme_enqueue_assets() {
   // Enqueue theme styles.
-  wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/build/css/style.css', array(), wp_get_theme()->get( 'Version' ) );
+  wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+
+  // Enqueue additional theme styles.
+  wp_enqueue_style( 'theme-custom-styles', get_template_directory_uri() . '/build/css/style.css', array( 'theme-styles' ), wp_get_theme()->get( 'Version' ) );
 
   // Enqueue theme scripts.
-  wp_enqueue_script( 'theme-enqueue_scripts', get_template_directory_uri() . '/build/js/main.js', array(), wp_get_theme()->get( 'Version' ), true );
+  wp_enqueue_script( 'theme-scripts', get_template_directory_uri() . '/build/js/main.js', array(), wp_get_theme()->get( 'Version' ), true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_assets' );
 
+// Enqueue styles for the editor.
+function theme_add_editor_styles() {
+  add_editor_style( 'build/css/style.css' );
+}
+add_action( 'after_setup_theme', 'theme_add_editor_styles' );
 
 // Allow SVG
 add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
