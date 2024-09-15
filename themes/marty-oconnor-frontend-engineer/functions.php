7f001416e48508891359c683c094bbf691b0cae4
martinDolan <martyopants@gmail.com>
2024-09-15 17:09:09 -0400

    Change style and script enqueing since i’m no longer using child theme

--- themes/marty-oconnor-frontend-engineer/functions.php
+++ themes/marty-oconnor-frontend-engineer/functions.php
@@ -3,19 +3,16 @@
 /**
  * Enqueue theme assets.
  */
+
 function theme_enqueue_assets() {
-	$parent_style = 'parent-style';
+  // Enqueue theme styles.
+  wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/build/css/style.css', array(), wp_get_theme()->get( 'Version' ) );
 
-	// Enqueue parent theme styles.
-	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-
-	// Enqueue theme styles.
-	wp_enqueue_style( 'theme-styles', get_stylesheet_directory_uri() . '/build/css/style.css', array( $parent_style ), wp_get_theme()->get( 'Version' ) );
-
-	// Enqueue theme scripts.
-	wp_enqueue_script( 'theme-enqueue_scripts', get_stylesheet_directory_uri() . '/build/js/main.js', array(), wp_get_theme()->get( 'Version' ), true );
+  // Enqueue theme scripts.
+  wp_enqueue_script( 'theme-enqueue_scripts', get_template_directory_uri() . '/build/js/main.js', array(), wp_get_theme()->get( 'Version' ), true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_assets' );
+
 
 // Allow SVG
 add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
